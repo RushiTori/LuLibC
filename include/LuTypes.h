38e1c8a425cffcad1f937696e88d41b7e15810f1
RushiTori <arcturusarc09@gmail.com>
2023-09-17 18:34:34 +0200

    added "byte" as uchar and "string" is now a define

--- include/LuTypes.h
+++ include/LuTypes.h
@@ -6,13 +6,14 @@
 #include <stdint.h>
 #include <time.h>
 
+typedef uint8_t byte;
 typedef uint8_t uchar;
 typedef uint16_t ushort;
 typedef uint32_t uint;
 typedef uint64_t ulong;
 typedef float flaot;
 
-typedef char* string;
+#define string char*
 typedef struct timespec Timer;
 
 #endif	// LU_TYPES
