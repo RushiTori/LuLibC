b0eed16e54e128c5446dd9c2cc3d24894f139ae7
RushiTori <Arcturusarc09@gmail.com>
2025-05-17 21:48:13 +0200

    removed unused macro

--- include/LuLogs.h
+++ include/LuLogs.h
@@ -5,11 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define __FILENAME__ (strrchr("/"__FILE__, '/') + 1)
-#define __FILENAME_LENGTH__                                                   \
-	(strrchr(__FILENAME__, '.') ? (strrchr(__FILENAME__, '.') - __FILENAME__) \
-								: strlen(__FILENAME__))
-
 #define LogMessage(fmt, ...) fprintf(stderr, fmt __VA_OPT__(, ) __VA_ARGS__)
 
 #define LogString(str) LogMessage("%s\n", str)
