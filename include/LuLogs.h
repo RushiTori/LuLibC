6a20a58ff673206d3a75ffbadfa0189daa6f1010
RushiTori <Arcturusarc09@gmail.com>
2025-10-11 12:11:15 +0200

    Added support for Windows errors logging Added more verbose macros Added getters and setters functions to change the target of the logs and if logging should happen Modified some macros to better reflect their names

--- include/LuLogs.h
+++ include/LuLogs.h
@@ -2,13 +2,101 @@
 #define LU_LOGS_H
 
 #include <errno.h>
+#include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-#define LogMessage(fmt, ...) fprintf(stderr, fmt __VA_OPT__(, ) __VA_ARGS__)
+#include "LuPlatforms.h"
+
+#if defined(WINDOWS)
+
+// Used by LuSocket.h
+#include <winsock2.h>
+
+// All purpose
+#include <windows.h>
+
+#endif	// Windows
+
+// Default: stderr.
+FILE* LuLogGetFile(void);
+
+// Won't set the log file if "file" is NULL
+void LuLogSetFile(FILE* file);
+
+// Default: true
+bool LuLogGetVerbose(void);
+
+// Sets if the LuLog system can outputs or not
+void LuLogSetVerbose(bool isVerbose);
+
+#define LogMessage(fmt, ...)                                         \
+	{                                                                \
+		if (LuLogGetVerbose()) {                                     \
+			fprintf(LuLogGetFile(), fmt __VA_OPT__(, ) __VA_ARGS__); \
+			fprintf(LuLogGetFile(), fmt __VA_OPT__(, ) __VA_ARGS__); \
+		}                                                            \
+	}
 
 #define LogString(str) LogMessage("%s\n", str)
 
-#define LogErrno(code) LogString(strerror(code))
+#define LogStringAndReturn(str, retVal) \
+	{                                   \
+		LogString(str);                 \
+		return retVal;                  \
+	}
+
+#define LogErrnoValue(errCode) LogString(strerror(errCode))
+
+#define LogErrnoValueAndReturn(errCode, retVal) \
+	{                                           \
+		LogErrnoValue(errCode);                 \
+		return retVal;                          \
+	}
+
+#define LogErrno() LogErrnoValue(errno)
+
+#define LogErrnoAndReturn(retVal) \
+	{                             \
+		LogErrno();               \
+		return retVal;            \
+	}
+
+#if defined(WINDOWS)
+
+void LogWSAErrorValue(DWORD errCode);
+
+#define LogWSAErrorValueAndReturn(errCode, retVal) \
+	{                                              \
+		LogWSAErrorValue(errCode);                 \
+		return retVal;                             \
+	}
+
+#define LogWSAError() LogWSAErrorValue(WSAGetLastError())
+
+#define LogWSAErrorAndReturn(retVal) \
+	{                                \
+		LogWSAError();               \
+		return retVal;               \
+	}
+
+void LogWindowsErrorValue(DWORD errCode);
+
+#define LogWindowsErrorValueAndReturn(errCode, retVal) \
+	{                                                  \
+		LogWindowsErrorValue(errCode);                 \
+		return retVal;                                 \
+	}
+
+#define LogWindowsError() LogWindowsErrorValue(GetLastError())
+
+#define LogWindowsErrorAndReturn(retVal) \
+	{                                    \
+		LogWindowsError();               \
+		return retVal;                   \
+	}
+
+#endif	// Windows
 
 #endif	// LU_LOGS_H
