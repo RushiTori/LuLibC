a24f8208b390c4df525db83780fed2379dda073b
RushiTori <Arcturusarc09@gmail.com>
2025-10-11 12:13:11 +0200

    Added type "SocketType" that represents the type of the return value of the function "socket" on both Windows  and Linux

--- include/LuTypes.h
+++ include/LuTypes.h
@@ -5,8 +5,22 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#include "LuPlatforms.h"
+
 typedef uint8_t byte;
+
 typedef uint16_t LuAngle;
+
 typedef int32_t (*CompareFunc)(const void*, const void*);
+
+#if defined(LINUX)
+typedef int64_t SocketType;
+#define LU_SOCKET_INVALID_SOCKET ((SocketType)(-1))
+#endif	// Linux
+
+#if defined(WINDOWS)
+typedef SOCKET SocketType;
+#define LU_SOCKET_INVALID_SOCKET ((SocketType)(INVALID_SOCKET))
+#endif	// Windows
 
 #endif	// LU_TYPES
